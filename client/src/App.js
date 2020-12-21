a2a2730145500167af52dd31a0246bdc50eb1a58
josh4500 <jibo.ajosh45@gmail.com>
2020-12-21 23:45:57 +0100

    ThemeContext deprecated

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
 import React, { useState } from "react";
 import Authentication from "./Authentication";
 import { UserProfile, NotePage } from "./components";
-import { AuthContext, ThemeContext, NoteContext } from "./context";
+import { ThemeContext } from "./context";
 import "./css/app.css";
 
 const App = () => {
   const [user, setUser] = useState({ active: true, data: {} });
-  return (
-    <ThemeContext.Consumer>
-      {(theme) => {
-        return user.active ? (
-          <div id="app">
-            <UserProfile user={user} notes={notes} theme={theme} />
-            <NotePage notes={notes} theme={theme} />
-          </div>
-        ) : (
-          <div id="authenticate">
-            <Authentication user={user} />
-          </div>
-        );
-      }}
-    </ThemeContext.Consumer>
+  const [notes, setNotes] = useState([]);
+  return user.active ? (
+    <div id="app">
+      <UserProfile user={user} notes={notes} theme={theme} />
+      <NotePage notes={notes} theme={theme} />
+    </div>
+  ) : (
+    <div id="authenticate">
+      <Authentication user={user} />
+    </div>
   );
 };
 
